59908ae1fef6f8011049d181e740d59ead31fab5
Eli Hinson <eli@Elis-MacBook-Pro.local>
2025-10-21 12:36:07 +0200

    added spoofDetected shared flag

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "YEOLiveness",
-      url: "https://github.com/Eli-YEO/YEOLiveness-SPM/releases/download/0.1.6/YEOLiveness.xcframework.zip",
-      checksum: "59a472c723e115e72a0f04ab98d80b2c7b335043313b62acf88dcb3e3e54c018"
+      url: "https://github.com/Eli-YEO/YEOLiveness-SPM/releases/download/0.1.7/YEOLiveness.xcframework.zip",
+      checksum: "91b1bcd648366e288fcb647873ff6d4cb33e8f6b761c94a8ac969a92e9f51098"
     )
   ]
 )
