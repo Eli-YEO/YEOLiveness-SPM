4ef28931aed6f3ff25c9d121d0e3cc28ba63468d
Eli Hinson <eli@Mac.lan>
2025-10-27 10:56:05 +0100

    cleaned up code a bit

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "YEOLiveness",
-      url: "https://github.com/Eli-YEO/YEOLiveness-SPM/releases/download/0.1.7/YEOLiveness.xcframework.zip",
-      checksum: "91b1bcd648366e288fcb647873ff6d4cb33e8f6b761c94a8ac969a92e9f51098"
+      url: "https://github.com/Eli-YEO/YEOLiveness-SPM/releases/download/0.1.8/YEOLiveness.xcframework.zip",
+      checksum: "5c149f50ed67619b1fe5244a497db84e956b5aaa68fa7690aa7c52044a1df4a0"
     )
   ]
 )
